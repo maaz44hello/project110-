c9afae43a17d14eab95c42f5ecce106d95433acc
maaz44hello <79090862+maaz44hello@users.noreply.github.com>
2022-06-09 19:19:35 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,49 @@
+import csv
+import plotly.figure_factory as ff
+import random
+import pandas as pd
+import statistics
+import plotly.graph_objects as go
+
+df = pd.read_csv("medium_data.csv")
+data = df ["reading_time"].tolist()
+
+def random_set_of_mean(counter):
+    dataset = []
+    for i in range(0,counter):
+        random_index = random.randint(0,len(data))
+        value = data[random_index]
+        dataset.append(value)
+    mean = statistics.mean(dataset)
+    return mean
+
+
+
+def show_fig(mean_list):
+    df = mean_list 
+    mean = statistics.mean(df)
+    fig = ff.create_distplot([df],["reading_time"],show_hist=False)
+    fig.add_trace(go.Scatter(x=[mean,mean],y=[0,1],mode="lines",name="MEAN"))
+    fig.show()
+
+
+def setup():
+    mean_list = []
+    for i in range(0,100):
+        set_of_means = random_set_of_mean(30)
+        mean_list.append(set_of_means)
+    show_fig(mean_list)
+
+    mean = statistics.mean(mean_list)
+    print("sampling mean:",mean)
+setup()
+
+#Code to find mean of raw vdata or population data
+
+population_mean = statistics.mean(data)
+print("Population mean:",population_mean)
+
+   
+
+
+
